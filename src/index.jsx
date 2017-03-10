4e6ef49ae7ddb3f559787cf8af53f8ec30caf611
CharmedSatyr <z@charmed.tech>
2017-03-10 22:51:24 +0900

    +source bootstrap folder; -more complex bootstrap folder

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import sass from './styles/styles.scss';
-import Bootstrap from './bootstrap/dist/css/bootstrap.css';
+import Bootstrap from './bootstrap-3.3.7-dist/css/bootstrap.min.css';
 //import BootstrapJS from './bootstrap/dist/js/bootstrap.min.jsx'; //Requires jQuery
 
 import App from './components/App.jsx';
