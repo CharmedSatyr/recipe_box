add7d8ca2abe17025bfc5ca06d79047360ca4125
CharmedSatyr <z@charmed.tech>
2017-03-16 03:22:27 +0900

    +Footer margin space; final tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,8 @@ class Footer extends React.Component {
                     to complete&nbsp;
                     <a href='https://freecodecamp.com' target='_blank'>freeCodeCamp</a>'s&nbsp;
                     <a href='https://www.freecodecamp.com/challenges/build-a-recipe-box' target='_blank'>Build a Recipe Box</a>&nbsp; challenge. Code licensed under&nbsp;
-                    <a href='https://github.com/CharmedSatyr/recipe_box/blob/master/LICENSE' target='_blank'>GPL-3.0</a>.
+                    <a href='https://github.com/CharmedSatyr/recipe_box/blob/master/LICENSE' target='_blank'>GPL-3.0</a>.&nbsp;
+                    <strong>Clearing your browser's cache will delete any saved recipes.</strong>
                 </footer>
             </div>
         )
