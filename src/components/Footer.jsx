31775c8d4ddd6c2ece324549aa957ef150508328
Joseph <z@charmed.tech>
2019-06-28 15:48:12 -0700

    Update all links to use new cs-fcc repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import G from '../img/G.png';
 //style='height: 24px'
 class Footer extends React.Component {
-    render() {
-        return (
-            <div className='container-fluid'>
-                <footer className='footer'>
-                    Designed and built by&nbsp;
-                    <a href='https://CharmedSatyr.com'>CharmedSatyr</a>
-                    <a href='https://github.com/CharmedSatyr/recipe_box/' target='_blank'>
-                        <img src={G}/>
-                    </a>
-                    to complete&nbsp;
-                    <a href='https://freecodecamp.com' target='_blank'>freeCodeCamp</a>'s&nbsp;
-                    <a href='https://www.freecodecamp.com/challenges/build-a-recipe-box' target='_blank'>Build a Recipe Box</a>&nbsp;challenge. Code licensed under&nbsp;
-                    <a href='https://github.com/CharmedSatyr/recipe_box/blob/master/LICENSE' target='_blank'>GPL-3.0</a>.&nbsp;
-                    <br/>
-                    <span className='glyphicon glyphicon-warning-sign'/>&nbsp;
-                    <strong>Clearing your browser's cache will delete any stored recipes.</strong>
-                </footer>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container-fluid">
+        <footer className="footer">
+          Designed and built by&nbsp;
+          <a href="https://CharmedSatyr.com">CharmedSatyr</a>
+          <a href="https://github.com/CharmedSatyr-freeCodeCamp/recipe_box/" target="_blank">
+            <img src={G} />
+          </a>
+          to complete&nbsp;
+          <a href="https://freecodecamp.com" target="_blank">
+            freeCodeCamp
+          </a>
+          's&nbsp;
+          <a href="https://www.freecodecamp.com/challenges/build-a-recipe-box" target="_blank">
+            Build a Recipe Box
+          </a>
+          &nbsp;challenge. Code licensed under&nbsp;
+          <a
+            href="https://github.com/CharmedSatyr-freeCodeCamp/recipe_box/blob/master/LICENSE"
+            target="_blank"
+          >
+            GPL-3.0
+          </a>
+          .&nbsp;
+          <br />
+          <span className="glyphicon glyphicon-warning-sign" />
+          &nbsp;
+          <strong>Clearing your browser's cache will delete any stored recipes.</strong>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default Footer;
